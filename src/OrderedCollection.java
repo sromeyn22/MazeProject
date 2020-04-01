9a49d93d1581aea1b78b89e7c77d5a7fc59f64e8
kma23-amherst <63013087+kma23-amherst@users.noreply.github.com>
2020-04-01 14:46:25 -0400

    open this file in intellij to edit

--- src/OrderedCollection.java
+++
@@ -1,6 +0,0 @@
-public abstract class OrderedCollection{
-    public abstract void append(Boolean ToAppend, int x, int y);
-    public abstract void pop();
-    public abstract int returnx(int X);
-    public abstract int returny(int Y);
-}
