40fa5edcb158d0d0ccac74076718807c8bd50170
kma23-amherst <63013087+kma23-amherst@users.noreply.github.com>
2020-04-07 14:38:47 -0400

    front page graphics update

--- MazeProject v2.0/src/FrontPageGraphics.java
+++ MazeProject v2.0/src/FrontPageGraphics.java
@@ -1,67 +1,68 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class FrontPageGraphics extends JPanel implements KeyListener, MouseListener {
+public class FrontPageGraphics {
     int dimensions;
     int typeOfMaze; //2 for 2D, 3 for 3D
     int key;
     JFrame frame;
-    JButton typeOfMazeButton;
-    JButton start;
-    JButton dimensionsAdd1;
-    JButton dimensionsAdd5;
-    JButton dimensionsMinus1;
-    JButton dimensionsMinus5;
 
     public FrontPageGraphics() {
-        addKeyListener(this);
-        addMouseListener(this);
-        this.setPreferredSize(new Dimension(500, 500));
-//        Thread mainThread = new Thread(new Runner());
-//        mainThread.start();
+        frame = new JFrame("Maze");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BufferedImage background = null;
+        try {
+            background = ImageIO.read(new File("maze.jpeg"));
+        } catch (IOException e){
+        }
+        frame.setContentPane(new ImagePanel(background));
+
+        JButton typeOfMazeButton;
+        JButton start;
+        JButton dimensionsAdd1;
+        JButton dimensionsAdd5;
+        JButton dimensionsMinus1;
+        JButton dimensionsMinus5;
+
+        JLabel label = new JLabel();
+        label.setForeground(Color.WHITE);
+        label.setText("label test");
+        label.setBounds(100, 100, 200, 50);
+        final JTextField tf=new JTextField();
+        tf.setBounds(200,200, 150,20);
+        JButton b = new JButton("change label");
+        b.setBounds(200, 300, 200, 50);
+        JButton addButton = new JButton( new AbstractAction("add") { @Override public void actionPerformed( ActionEvent e ) {tf.setText("added"); } });
+        addButton.setBounds(200, 400, 200, 50);
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText(b.getText());
+            }
+        });
+        frame.add(b);
+        frame.add(addButton);
+        frame.add(tf);
+        frame.add(label);
+        frame.setSize(1024, 768);
+        frame.setVisible(true);
     }
+}
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-
+class ImagePanel extends JComponent {
+    private Image image;
+    public ImagePanel(Image image) {
+        this.image = image;
     }
-
     @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
     }
 }
--- MazeProject v2.0/src/Main.java
+++ MazeProject v2.0/src/Main.java
@@ -2,14 +2,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main (String[] args){
-//        JFrame frame = new JFrame("Maze");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        FrontPageGraphics mainInstance = new FrontPageGraphics();
-//        frame.setContentPane(mainInstance);
-//        frame.pack();
-//        frame.setVisible(true);
 
-        JButton addButton = new JButton( new AbstractAction("add") { @Override public void actionPerformed( ActionEvent e ) { // add Action } });
-            JButton substractButton = new JButton( new AbstractAction("substract") { @Override public void actionPerformed( ActionEvent e ) { // substract Action } });
+        FrontPageGraphics frontPage = new FrontPageGraphics();
+        frontPage.frame.setVisible(true);
     }
 }
